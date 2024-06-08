a223ea1cc8454175b37e9b7846ce656b80662cfa
kabira semlal <semkabira.com>
2024-06-08 22:25:37 +0100

    updat navbar 2

--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -1,43 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBuilding, faUser, faUsers, faIndustry, faBook, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';  // Importez le fichier CSS
-import logo from '../assets/Syndic.png'
+import logo from '../assets/logoS.png';
+
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg col-3">
             <Link className="navbar-brand" to="/">
-            <img src={logo} className="img-fluid rounded-circle" alt="Logo Syndic Gestion" />
-                Syndic Gestion
-                
+                <img src={logo} className="img-fluid rounded-circle" alt="Logo Syndic Gestion" />
             </Link>
             <div className='ligne'></div>
-            {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button> */}
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Accueil</Link>
+                        <Link className="nav-link" to="/">
+                            <FontAwesomeIcon icon={faHome} /> Accueil
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/appartements">Appartements</Link>
+                        <Link className="nav-link" to="/appartements">
+                            <FontAwesomeIcon icon={faBuilding} /> Appartements
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/owners">Owners</Link>
+                        <Link className="nav-link" to="/owners">
+                            <FontAwesomeIcon icon={faUser} /> Owners
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/employes">Employes</Link>
+                        <Link className="nav-link" to="/employes">
+                            <FontAwesomeIcon icon={faUsers} /> Employes
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/companys">Companys</Link>
+                        <Link className="nav-link" to="/companys">
+                            <FontAwesomeIcon icon={faIndustry} /> Companys
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/blooks">Blooks</Link>
+                        <Link className="nav-link" to="/blooks">
+                            <FontAwesomeIcon icon={faBook} /> Blooks
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/reports">Reports</Link>
+                        <Link className="nav-link" to="/reports">
+                            <FontAwesomeIcon icon={faChartBar} /> Reports
+                        </Link>
                     </li>
-                    
                 </ul>
             </div>
         </nav>
